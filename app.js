0a747e170db45f830bc6bc7f11f122a16f3f863c
AhmedRaza186 <sallumoon650@gmail.com>
2025-12-01 17:14:23 +0500

    ui completed

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-let num;
-function generateNum(){
-     num = Math.ceil(Math.random() * 20)
-console.log(num)
-}
+// let num;
+// function generateNum(){
+//      num = Math.ceil(Math.random() * 20)
+// console.log(num)
+// }
 
-generateNum()
+// generateNum()
 
-let p = document.querySelector('.showText h2')
+// let p = document.querySelector('.showText h2')
 
-let guessArr = []
+// let guessArr = []
 
-document.querySelector('button').addEventListener('click',checkNum)
+// document.querySelector('button').addEventListener('click',checkNum)
 
-function checkNum(){
-    let input = document.querySelector('input')
-let userGuess = +(input.value)
-input.value = ''
-console.log(userGuess)
-guessArr.push(userGuess)
-if(userGuess == num){
-    alert("You Won")
-}
-else if (guessArr.length == 5){
-    setTimeout(() => {
-        alert('You lose')
-    },1000)
-      generateNum()
-      guessArr = []
+// function checkNum(){
+//     let input = document.querySelector('input')
+// let userGuess = +(input.value)
+// input.value = ''
+// console.log(userGuess)
+// guessArr.push(userGuess)
+// if(userGuess == num){
+//     alert("You Won")
+// }
+// else if (guessArr.length == 5){
+//     setTimeout(() => {
+//         alert('You lose')
+//     },1000)
+//       generateNum()
+//       guessArr = []
       
-}
+// }
 
-let diff = num - userGuess
-console.log(diff)
-if(diff == 1){
-    p.innerHTML = 'You are Low but very very Close'
-    return
-}
-else if(diff == -1){
-    p.innerHTML = 'You are high but very very Close'
-    return
+// let diff = num - userGuess
+// console.log(diff)
+// if(diff == 1){
+//     p.innerHTML = 'You are Low but very very Close'
+//     return
+// }
+// else if(diff == -1){
+//     p.innerHTML = 'You are high but very very Close'
+//     return
     
-}
-if(diff > 0 && diff <= 3){
-    p.innerHTML = 'You are Low but Close'
-    return
+// }
+// if(diff > 0 && diff <= 3){
+//     p.innerHTML = 'You are Low but Close'
+//     return
 
-}
-else if(diff < 0 && diff >= -3){
-    p.innerHTML = 'You are high but Close'
-    return
+// }
+// else if(diff < 0 && diff >= -3){
+//     p.innerHTML = 'You are high but Close'
+//     return
     
-}
+// }
 
 
-}
+// }
