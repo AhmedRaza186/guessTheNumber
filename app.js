5ef09ae71320ed7c359abc0d0c26dbb67e429788
AhmedRaza186 <sallumoon650@gmail.com>
2025-12-03 17:08:07 +0500

    almost completed

--- app.js
+++ app.js
@@ -1,56 +1,145 @@
-// let num;
-// function generateNum(){
-//      num = Math.ceil(Math.random() * 20)
-// console.log(num)
-// }
+let loseSound = new Audio('./gameover.mp3')
+let bgSound = new Audio('./background.mp3')
+let newRound = new Audio('./newround.mp3')
+let winGame = new Audio('./wingame.mp3')
+let winRound = new Audio('./winround.mp3')
 
-// generateNum()
+bgSound.play()
+let num;
+function generateNum() {
+    num = Math.ceil(Math.random() * 20)
+    console.log(num , '===> num')
+}
 
-// let p = document.querySelector('.showText h2')
+generateNum()
 
-// let guessArr = []
-
-// document.querySelector('button').addEventListener('click',checkNum)
-
-// function checkNum(){
-//     let input = document.querySelector('input')
-// let userGuess = +(input.value)
-// input.value = ''
-// console.log(userGuess)
-// guessArr.push(userGuess)
-// if(userGuess == num){
-//     alert("You Won")
-// }
-// else if (guessArr.length == 5){
-//     setTimeout(() => {
-//         alert('You lose')
-//     },1000)
-//       generateNum()
-//       guessArr = []
-      
-// }
-
-// let diff = num - userGuess
-// console.log(diff)
-// if(diff == 1){
-//     p.innerHTML = 'You are Low but very very Close'
-//     return
-// }
-// else if(diff == -1){
-//     p.innerHTML = 'You are high but very very Close'
-//     return
-    
-// }
-// if(diff > 0 && diff <= 3){
-//     p.innerHTML = 'You are Low but Close'
-//     return
-
-// }
-// else if(diff < 0 && diff >= -3){
-//     p.innerHTML = 'You are high but Close'
-//     return
-    
-// }
+document.querySelector('.again').addEventListener('click',() => {
+    window.location.reload() 
+})
+let guessingStatus = document.querySelector('.guessing')
+let userGuessDisplay = document.querySelector('.box p')
 
 
-// }
+let turnDisplay = document.querySelector('.turns')
+
+let scoreMultipler = 0
+
+let round = 0
+let roundDisplay = document.querySelector('#rounds')
+function roundChecker() {
+    round++
+    if(round <=2){
+        scoreMultipler = 2
+        turns = 10
+    }
+    else if(round <=4){
+        scoreMultipler = 4
+        turns = 5
+    }
+    else if(round <=6){
+        scoreMultipler = 6.66
+        turns = 3
+    }
+    else if(round <=9){
+        scoreMultipler = 10
+        turns = 2
+    }
+    else if(round =10){
+        scoreMultipler = 20
+        turns = 1
+    }
+
+    roundDisplay.innerText = `Round ${round}`
+    userGuessDisplay.innerText = `?`
+    guessingStatus.innerText = 'Start Guessing....'
+
+
+}
+roundChecker()
+let scoreDisplay = document.querySelector('.score')
+
+let score = 0
+function scoreChecker(turns){
+ score = turns * scoreMultipler
+console.log(scoreMultipler , '===> multiplyer')
+console.log(turns , '===> scoreCheckerturn')
+console.log(score , '===> score')
+scoreDisplay.innerText = `Score :${Math.round(score)}`
+}
+let totalScore = 0
+let totalScoreDisplay = document.querySelector('.totalScore')
+
+function totalScoreHandler(){
+totalScore += score
+totalScoreDisplay.innerText = `Total Score :${Math.round(totalScore)}`
+}
+
+
+document.querySelector('.check').addEventListener('click', checkNum)
+
+function checkNum() {
+    let input = document.querySelector('input')
+    let userGuess = +(input.value)
+    if(!Number(userGuess)){
+        alert(`Only Numbers allowed`)
+        return
+    }
+    userGuessDisplay.innerText = userGuess
+    input.value = ''
+    turns--
+    if (userGuess == num) {
+        if(round == 10){
+            alert('You win the whole game ')
+            round = 1
+            return
+        }
+        alert("You Won")
+        generateNum()
+        scoreChecker(turns + 1)
+        totalScoreHandler()
+        roundChecker()
+        console.log(turns)
+        userGuessDisplay.innerText = `?`
+        guessingStatus.innerText = 'Start Guessing....'
+        turnDisplay.innerText = `Remaining Turns :${turns}`
+        return
+
+    }
+
+    console.log(turns , '===> turn')
+    turnDisplay.innerText = `Remaining Turns :${turns}`
+    console.log(turnDisplay , '===> turn display')
+    if (turns < 1) {
+        setTimeout(() => {
+            alert('You lose')
+        }, 1000)
+        generateNum()
+        round = 1
+        turnsArr = []
+
+
+    }
+
+    let diff = num - userGuess
+
+
+    if (diff > 0 && diff <= 3) {
+        guessingStatus.innerText = 'You are Low but Close'
+        return
+
+    }
+    else if (diff < 0 && diff >= -3) {
+        guessingStatus.innerText = 'You are high but Close'
+        return
+
+    }
+    else if (diff > 3) {
+        guessingStatus.innerText = 'You are Low'
+        return
+    }
+    else if (diff < -3) {
+        guessingStatus.innerText = 'You are High'
+        return
+
+    }
+}
